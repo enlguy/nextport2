dea06bede9f8cd90a5eb764224a0dfbdb2ecdc42
Jack <jack282323@gmail.com>
2024-01-08 14:44:37 +0100

    Adding analytics

--- app/page.js
+++ app/page.js
@@ -11,6 +11,10 @@ const TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
 ReactGA.initialize(TRACKING_ID);
 
 export default function Home() {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
+
   return (
     <main className="flex min-h-screen flex-col container mx-auto bg-[#424040]">
       <Navbar />
