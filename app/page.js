086df7eca8a6bcac0073a531caff1a22cd9b848e
Jack <jack282323@gmail.com>
2024-01-11 18:34:53 +0100

    Fixed form

--- app/page.js
+++ app/page.js
@@ -5,17 +5,8 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
-import MyApp from "./_app";
-import ReactGA from "react-ga";
-
-const TRACKING_ID = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS;
-ReactGA.initialize(TRACKING_ID);
 
 export default function Home() {
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname + window.location.search);
-  }, []);
-
   return (
     <main className="flex min-h-screen flex-col container mx-auto bg-[#424040]">
       <Navbar />
