6f9fd6b2c9b5b7729ffb0e611e77721975f635a3
Jack <jack282323@gmail.com>
2024-02-01 14:20:21 +0100

    Adding interview sources section

---
+++ app/components/Appearances.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import Image from "next/image";
+
+const Appearances = () => {
+  return (
+    <>
+      <span className="items-center justify-between text-center text-xl text-bold">
+        I've been interviewed by:
+      </span>
+      <div className="items-center justify-between flex">
+        <Image
+          src="/amag.jpg"
+          alt="Authority Magazine logo"
+          width={200}
+          height={200}
+        />
+        <Image
+          src="/Newsweek_Logo.svg"
+          alt="newsweek logo"
+          width={300}
+          height={300}
+        />
+        <Image
+          src="/nerdwalletlogo.svg"
+          alt="Authority Magazine logo"
+          width={300}
+          height={300}
+        />
+      </div>
+      \
+    </>
+  );
+};
+
+export default Appearances;
--- app/page.js
+++ app/page.js
@@ -5,12 +5,14 @@ import About from "./components/About";
 import Projects from "./components/Projects";
 import EmailSection from "./components/EmailSection";
 import Footer from "./components/Footer";
+import Appearances from "./components/Appearances";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col container mx-auto bg-gradient-to-b from bg-medblue to bg-lgtblue">
       <Navbar />
       <Hero />
+      <Appearances />
       <About />
       <Projects />
       <EmailSection />
