d8b70b18b1264db1d9194efacf817705d2095fe9
Jack <jack282323@gmail.com>
2024-02-01 14:34:01 +0100

    Small changes to footer size and text in contact

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer border z-10 border-t-[#33353F] border-l-transparent border-r-transparent text-white">
-      <div className="container p-12 flex justify-between">
-        <p className="text-slate-600">All rights reserved 2023.</p>
+      <div className="container p-10 flex justify-between">
+        <p className="text-slate-600">All rights reserved 2024</p>
       </div>
     </footer>
   );
