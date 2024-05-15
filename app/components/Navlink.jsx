3ab6c30605ba65b191fb83e9e17ade2d2b4a12cc
Jack <jack282323@gmail.com>
2024-05-15 20:23:59 +0200

    Update Navlink.jsx

--- app/components/Navlink.jsx
+++ app/components/Navlink.jsx
@@ -5,6 +5,7 @@ const Navlink = ({ href, title }) => {
     <a>
       <Link
         href={href}
+        prefetch={false}
         className="block py-2 pl-3 pr-4 text-[#e3ebf0] sm:text-xl rounded md:p-0 hover:text-white"
       >
         {title}
